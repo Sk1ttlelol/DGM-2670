1fbda5f8d7363aaafa8961c3c681e65e64bb5bd1
Darick Cochrane <boomrocks24@gmail.com>
2021-09-22 01:25:33 -0600

    Started on WaveSpawner and enemies

---
+++ DGM 2670 game to publish/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Wave
+{
+    public string waveName;
+    public int noOfEnemies;
+    public GameObject[] typeOfEnemies;
+    public float spawnInterval;
+}
+public class WaveSpawner : MonoBehaviour
+{
+    public Wave[] waves;
+    public Transform[] spawnPoints;
+
+    private Wave currentWave;
+    private int currentWaveNumber;
+
+    private void Update()
+    {
+        currentWave = waves[currentWaveNumber];
+        SpawnWave();
+    }
+
+    void SpawnWave()
+    {
+        GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+        Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+    }
+
+
+}
