811af1587326622cd31af79e453240fe39ae4971
Darick Cochrane <boomrocks24@gmail.com>
2021-09-24 00:53:56 -0600

    Created art switcher for collections, looking to implement for enemy prefab

--- DGM 2670 game to publish/Assets/Scripts/Anthony's Tuts/CollectionBehaviour.cs
+++ DGM 2670 game to publish/Assets/Scripts/Anthony's Tuts/CollectionBehaviour.cs
@@ -1,15 +1,39 @@
+using System;
 using UnityEngine;
 
 namespace Anthony_s_Tuts
 {
     public class CollectionBehaviour : MonoBehaviour
     {
+        private GameObject art;
+        private SpriteRenderer artSpriteRenderer;
         public CollectableSO collectedObj;
         public CollectionSO collection;
-        private void Start()
+
+        private void Awake()
         {
+           ConfigCollectable();
+        }
+
+        public void SwapCollectable(CollectableSO collectable)
+        {
+            collectedObj = collectable;
+            ConfigCollectable();
+        }
+
+        private void ConfigCollectable()
+        {
+            art = GetComponentInChildren<Transform>().gameObject;
+            artSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            if (artSpriteRenderer != null)
+            {
+                artSpriteRenderer.sprite = collectedObj.art2D;
+                artSpriteRenderer.color = collectedObj.artColorTint;
+            }
+           
             EnableDisableCollectable(!collectedObj.collected);
         }
+
 
         private void OnTriggerEnter(Collider other)
         {
@@ -19,7 +43,7 @@ namespace Anthony_s_Tuts
 
         private void EnableDisableCollectable(bool value)
         {
-            GetComponent<MeshRenderer>().enabled = value;
+            art.SetActive(value);
             GetComponent<Collider>().enabled = value;
         }
     }
