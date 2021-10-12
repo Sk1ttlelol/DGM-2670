4b36848c34d75cc4f0e1e5bc3b5f8e11598d8f1f
Darick Cochrane <boomrocks24@gmail.com>
2021-10-11 18:59:22 -0600

    misc

---
+++ DGM 2670 game to publish/Assets/Scripts/Anthony's Tuts/ImageBehaviour.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ImageBehaviour : MonoBehaviour
+{
+    private Image imageObj;
+    private void Start()
+    {
+        //imageObj
+    }
+
+   
+}
