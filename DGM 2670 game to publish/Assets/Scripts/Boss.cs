f3b142c25cbfd9c548ed1e1a07bc977a29e63a22
Darick Cochrane <boomrocks24@gmail.com>
2021-10-18 23:20:07 -0600

    Boss movement and fight mostly finished

--- DGM 2670 game to publish/Assets/Scripts/Boss.cs
+++ DGM 2670 game to publish/Assets/Scripts/Boss.cs
@@ -1,19 +1,91 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Boss : MonoBehaviour
 {
+    public Vector3 spawnPos;
 
-    public int health;
-    public int damage;
+    public float topBound = 5.4f, botBound = -3.55f, sideBound = 1.8f;
+    
+    private bool bossInArena = false;
+    
+    private bool topHit = true, botHit = false, leftHit = false, rightHit = true;
 
-    public Slider healthbar;
+
+    public float speed;
+    
 
     private void Update()
     {
-        healthbar.value = health;
+      
+        if (bossInArena == false)
+        {
+            transform.position = spawnPos;
+            bossInArena = true;
+            Debug.Log("I should only see this once");
+        }
+        else
+        {
+            bossInArena = true;
+        }
+
+        if (topHit == true)
+        {
+            botHit = false;
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+        
+        if (botHit == true)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        
+        if (rightHit == true)
+        {
+            leftHit = false;
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
+        if (leftHit == true)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        
+        if (transform.position.y >= topBound)
+        {
+            topHit = true;
+        }
+        
+        if (transform.position.y <= botBound)
+        {
+            botHit = true;
+            topHit = false;
+        }
+        
+        if (transform.position.x >= sideBound)
+        {
+            rightHit = true;
+        }
+        
+        if (transform.position.x <= -sideBound)
+        {
+            leftHit = true;
+            rightHit = false;
+        }
+
+        
+        
+        
+        
+        
+        
+        
+        
+        Debug.Log("I should see this a lot");
+        
     }
+    
 }
+
