48142408d539e3cf4f8a6c53c8a6d492a4cdc8c0
Darick Cochrane <boomrocks24@gmail.com>
2021-10-25 14:12:52 -0600

    Enemy Ship art and Asteroids added. Created Laser art for player ship

---
+++ DGM 2670 game to publish/Assets/Scripts/DataStorage.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataStorage : ScriptableObject
+{
+   public ScriptableObject data;
+   public List<ScriptableObject> listData;
+
+   public void SetListData()
+   {
+      
+   }
+}
