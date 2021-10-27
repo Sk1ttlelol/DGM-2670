cd64a9255b38935cdef1295347c2245e469c2f24
Darick Cochrane <boomrocks24@gmail.com>
2021-10-27 11:08:51 -0600

    AnimState tut

---
+++ DGM 2670 game to publish/Assets/Scripts/Anthony's Tuts/AnimStates.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimStates : StateMachineBehaviour
+{
+  public UnityEvent enterStateEvent;
+
+  override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+  {
+    enterStateEvent.Invoke();
+  }
+}
